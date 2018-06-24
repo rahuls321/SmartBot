86de6e847561ad104dc137463a090a0ad22f0ae5
rahul <kumarsrahul123@gmail.com>
2018-06-24 23:20:29 +0530

    Modify Back-End

--- src/Components/Rank/Rank.js
+++ src/Components/Rank/Rank.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const Rank = () => {
+const Rank = ({name, entries}) => {
   return (
     <div>
       <div className='white f3'>
-        {'Rahul, your current rank is ..'}
+        {`${name} , your current entry count ...`}
       </div>
-      <div className='white f1'>
-        {'#5'}
-      </div> 
+      <div className='white f1 '>
+        {entries}
+      </div>
     </div>
   );
 }
